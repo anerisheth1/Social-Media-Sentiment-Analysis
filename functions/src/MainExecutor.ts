6c51c5971a1033e5e08c40ce53756ddac3a8c8d4
Vishnu Rajan Tejus <vrtejus@gmail.com>
2023-06-17 15:00:43 -0700

    second commit

--- functions/src/MainExecutor.ts
+++ functions/src/MainExecutor.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from "express";
 import * as admin from "firebase-admin";
 import ytdl, { videoInfo } from "ytdl-core";
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
+import { Storage } from "@google-cloud/storage";
+import * as fs from "fs";
+
+dotenv.config();
 
 admin.initializeApp();
 
 const app = express();
+const storage = new Storage();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 app.post("/download", async (req: Request, res: Response) => {
   try {
@@ -32,7 +45,7 @@ app.post("/download", async (req: Request, res: Response) => {
       .pipe(videoWriteStream);
 
     // Download audio only
-    const audioFilePath = `hackathon_videos_from_download_api/${baseFileName}_A.mp3`;
+    const audioFilePath = `hackathon_videos_from_download_api/${baseFileName}_A.flac`;
     const audioFile = admin.storage().bucket().file(audioFilePath);
     const audioWriteStream = audioFile.createWriteStream();
 
@@ -55,10 +68,18 @@ app.post("/download", async (req: Request, res: Response) => {
         audioWriteStream.on("error", reject);
       }),
     ])
-      .then(() => {
+      .then(async () => {
+        const transcript = await transcribeAudio(
+          audioFile.bucket.name,
+          audioFilePath
+        );
+
+        console.log("transcript", transcript);
+
         res.json({
           videoURI: gsVideoURI,
           audioURI: gsAudioURI,
+          transcript: transcript,
         });
       })
       .catch((error) => {
@@ -68,6 +89,57 @@ app.post("/download", async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while downloading the video");
+  }
+});
+
+const transcribeAudio = async (bucketName: string, filePath: string) => {
+  try {
+    const downloadedFile = await getFileFromStorage(bucketName, filePath);
+
+    // Pass the audioFile object to the createTranscription function
+    const resp = await openai.createTranscription(downloadedFile, "whisper-1");
+    return resp;
+  } catch (e) {
+    console.error("[transcribeAudio] error: ", e);
+    return null;
+  }
+};
+
+const getFileFromStorage = async (
+  bucketName: string,
+  filePath: string
+): Promise<File> => {
+  try {
+    const file = storage.bucket(bucketName).file(filePath);
+    const tempFilePath = `/tmp/${Date.now()}_${Math.floor(
+      Math.random() * 10000
+    )}`;
+    await file.download({ destination: tempFilePath });
+    const fileBuffer = fs.readFileSync(tempFilePath);
+    const blob = new Blob([fileBuffer]);
+    return new File([blob], filePath);
+  } catch (error) {
+    console.error("Error fetching file from Firebase Storage:", error);
+    throw error;
+  }
+};
+
+app.post("/transcribe", async (req: Request, res: Response) => {
+  try {
+    const { audioURI } = req.body;
+
+    if (!audioURI) {
+      throw new Error("Audio URI is required");
+    }
+
+    const [bucketName, filePath] = audioURI.split("/").slice(2);
+    const downloadedFile = await getFileFromStorage(bucketName, filePath);
+
+    // Pass the audioFile object to the createTranscription function
+    const resp = await openai.createTranscription(downloadedFile, "whisper-1");
+    res.json(resp);
+  } catch (e) {
+    console.error("[transcribeAudio] error: ", e);
   }
 });
 
